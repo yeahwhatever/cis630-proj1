4f4b41046cf09aa4d53af0db3e241d50367689e6
Joe Pletcher <joepletcher@gmail.com>
2011-12-03 22:54:45 -0800

    First shot at a worker function

---
+++ p.c
@@ -0,0 +1,33 @@
+
+void row_worker(float data[], int l) {
+	int row_length = l, i;
+	float **sheet;
+	float *ret;
+
+	ret = xmalloc(row_length * sizeof(float));
+
+	sheet = xmalloc(3 * sizeof(float *));
+
+	for (i = 0; i < 3; i++)
+		sheet[i] = xmalloc(row_length * sizeof(float));
+
+
+	for (i = 0; i < row_length; i++) {
+		sheet[0][i] = data[i];
+		sheet[1][i] = data[row_length + i];
+		sheet[2][i] = data[row_length * 2 + i];
+	}
+
+	for (i = 1; i < row_length - 1; i++)
+		ret[i] = (sheet[0][i] + sheet[2][i] + sheet[1][i-1] + sheet[1][i] + sheet[1][i+i]) / 5.0;
+
+
+	/* send ret */
+
+	free(ret);
+
+	for (i = 0; i < 3; i++)
+		free(sheet[i]);
+
+	free(sheet);
+}
