2c440e195b280a170adad8606cde17bd02265172
Adam Bates <adam@home.(none)>
2011-12-04 20:53:15 -0800

    Added junk to common.c to generate random heatpoints (probably incorrectly)

--- common.h
+++ common.h
@@ -1,6 +1,9 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#define MAX_HEAT 1000
+#define MIN_HEAT -100
+
 struct heatpoint {
 	int x;
 	int y;
@@ -26,4 +29,6 @@ char* mkJson(int x, int y, int num_heat, struct heatpoint *hps );
 char* mkJsonQuery(int x, int y);
 struct heatpoint *parseJson(char* json, int *width, int *height, int *num_heat);
 void parseJsonQuery(char* json, int *x, int *y);
+
+
 #endif
