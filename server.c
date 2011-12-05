d805699cdc57df0f79969d2b37eb616c348a58a1
nt-cmplt <ntiller@cs.uoregon.edu>
2011-12-04 18:05:20 -0800

    added p.c's contents to server.c

--- server.c
+++ server.c
@@ -260,10 +260,50 @@ void slave_compute() {
 
 
 	while (1) {
-	MPI_Recv(&len, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &stat);
+		MPI_Recv(&len, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &stat);
 
 	}
 
+}
+
+void row_worker(float data[], int l) {
+	int row_length = l, i;
+	float **sheet;
+	float *ret;
+
+	ret = xmalloc(row_length * sizeof(float));
+
+	sheet = xmalloc(3 * sizeof(float *));
+
+	if (tag == WORK) {
+
+		for (i = 0; i < 3; i++)
+			sheet[i] = xmalloc(row_length * sizeof(float));
+
+
+		for (i = 0; i < row_length; i++) {
+			sheet[0][i] = data[i];
+			sheet[1][i] = data[row_length + i];
+			sheet[2][i] = data[row_length * 2 + i];
+		}
+
+		ret[0] = 0;
+		ret[row_length - 1] = 0;
+
+		for (i = 1; i < row_length - 1; i++)
+			ret[i] = (sheet[0][i] + sheet[2][i] + sheet[1][i-1] + sheet[1][i] + sheet[1][i+i]) / 5.0;
+
+
+		/* send ret */
+
+	} else if (tag == DIE) {
+		free(ret);
+
+		for (i = 0; i < 3; i++)
+			free(sheet[i]);
+
+		free(sheet);
+	}
 }
 
 void gen_minisheet(int start_x, float *s, float *minisheet) {
